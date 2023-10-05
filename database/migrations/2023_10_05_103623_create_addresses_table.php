211568eb345bd834cae56c16059b250f6bc1808c
Naseef <AUNaseef@users.noreply.github.com>
2023-10-05 16:59:44 +0530

    initialize objects

---
+++ database/migrations/2023_10_05_103623_create_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['billing', 'shipping']);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company')->nullable();
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('city');
+            $table->string('zip');
+            $table->string('state');
+            $table->string('phone');
+            $table->timestamps();
+
+            // Compound unique constraint on user_id and type
+            $table->unique(['user_id', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
