c49e4d35b29b2cbcf5bf4ed6cb18c3d8f047cf2e
Nawodi_Priyawansha <n.t.priyawansha@gmail.com>
2023-10-07 10:14:10 +0530

    Update checkoutDetails.blade.php
    added order details vies screen

--- resources/views/layouts/client/checkoutDetails.blade.php
+++ resources/views/layouts/client/checkoutDetails.blade.php
@@ -3,171 +3,131 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Include the necessary Tailwind CSS file -->
-    <!-- Example: <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Order Details</title>
 
-    <style>
-        .checkout-bg {
-            background-image: url("{{ asset('images/composition-cleaning-objects-with-copyspace@0.5x.jpg') }}");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position:  25% 75%; /* Adjust the background position */
-            text-align: center;
-            padding: 100px; /* Adjust the padding as needed */
-            color: white; /* Text color on top of the background */
-        }
-        
-        .checkout-text {
-            @apply text-red-500 font-bold text-center md:mt-10;
-            font-size: 48px; /* Increase the font size for larger text */
-            text-decoration: underline; /* Add an underline */
-        }
-        
-        .thank-you-text {
-            text-align: left; /* Align the text to the left */
-            margin-left: 20px; /* Add left margin for spacing */
-            font-size: 24px; /* Adjust the font size as needed */
-        }
-        
-        .order-table {
-            @apply w-full mt-4 border-collapse border border-gray-300;
-        }
-        
-        /* Add a dotted border to all table cells */
-        .order-table th,
-        .order-table td {
-            border-right: 1px dotted #ccc; /* Add a dotted border on the right side of each cell */
-        }
-        
-        /* Remove the border on the last cell in each row to avoid extra lines */
-        .order-table th:last-child,
-        .order-table td:last-child {
-            border-right: none;
-        }
-
-        /* Apply classes to style the first row differently */
-        .order-table thead tr {
-            /* Normal style for column headers */
-        }
-        
-        /* Apply classes to style column headers differently */
-        .order-table th {
-            @apply font-normal;
-        }
-
-        /* Apply a class to style the content rows differently */
-        .content-row {
-            @apply bg-gray-700 text-white; /* Dark background and white text */
-        }
-
-        /* Style for larger "Order Details" text */
-        .order-details-text {
-            font-size: 36px; /* Adjust the font size for larger text */
-            font-weight: bold; /* Make the text bold */
-        }
-
-        /* Style for vertical alignment */
-        .vertical-table {
-            display: flex;
-            flex-direction: column;
-            gap: 10px; /* Adjust the gap between items */
-        }
-
-        /* Style for vertical alignment of labels */
-        .label {
-            font-weight: bold;
-        }
-
-        /* Style for the billing address box */
-        .billing-address-box {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-top: 20px;
-        }
-
-        /* Style for the billing address heading */
-        .billing-address-heading {
-            font-weight: bold;
-            font-size: 24px;
-        }
-    </style>
-    <title>Document</title>
 </head>
 <body>
-    <h1 class="checkout-text checkout-bg">
-        Checkout
-    </h1>
-    <div class="thank-you-text">
-        Thank you. Your order has been received.
+    <div class="container min-w-fit mx-auto mt-10 p-4 sm:px-6 md:px-8 w-6/12">
+        <!-- Order place details -->
+        <div class="pb-5">
+            <p class="text-gray-500">Order #<span id="orderNumber" class="text-black">1695</span> was placed on <span id="orderDate" class="text-black">October 6, 2023</span> and is currently <span id="orderStatus" class="text-black">On hold</span>.</p>
+        </div>
+        <!-- Heading -->
+    <div class="mb-5">
+        <h1 class="text-5xl">
+            Order details
+        </h1>
     </div>
-    <table class="order-table">
-        <thead>
-            <tr>
-                <th class="w-1/5">ORDER NUMBER:</th>
-                <th class="w-1/5">DATE:</th>
-                <th class="w-1/5">EMAIL:</th>
-                <th class="w-1/5">TOTAL:</th>
-                <th class="w-1/5">PAYMENT METHOD:</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="content-row">
-                <td class="w-1/5">12345</td>
-                <td class="w-1/5">2023-10-10</td>
-                <td class="w-1/5">example@email.com</td>
-                <td class="w-1/5">$100.00</td>
-                <td class="w-1/5">Credit Card</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <!-- "Order Details" table with vertical alignment -->
-    <h2 class="order-details-text">Order Details</h2>
-    <table class="vertical-table">
-        <tr>
-            <td class="label">Order Number:</td>
-            <td>12345</td>
-        </tr>
-        <tr>
-            <td class="label">Date:</td>
-            <td>2023-10-10</td>
-        </tr>
-        <tr>
-            <td class="label">Email:</td>
-            <td>example@email.com</td>
-        </tr>
-        <tr>
-            <td class="label">Product:</td>
-            <td>8848 Test Product 06 × 1</td>
-        </tr>
-        <tr>
-            <td class="label">Total:</td>
-            <td>$47.00</td>
-        </tr>
-        <tr>
-            <td class="label">Subtotal:</td>
-            <td>$47.00</td>
-        </tr>
-        <tr>
-            <td class="label">Payment Method:</td>
-            <td>Direct bank transfer</td>
-        </tr>
-    </table>
-
+    <!-- Order Details -->
+    <div class="relative overflow-x-auto border border-gray-300">
+        <!-- Oder Items -->
+        <div class="px-5">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Product
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Total
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-blue-500 whitespace-nowrap dark:text-white w-6/12">
+                            8848 Test Product 04
+                            <span class="text-gray-500">x</span>
+                            <span class="text-gray-500">1</span>
+                        </th>
+                        <td class="px-6 py-4 w-6/12">
+                            $10.30
+                        </td>
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-blue-500 whitespace-nowrap dark:text-white">
+                            8848 Test Product 04
+                            <span class="text-gray-500">x</span>
+                            <span class="text-gray-500">1</span>
+                        </th>
+                        <td class="px-6 py-4">
+                            $10.30
+                        </td>
+                    </tr>
+                     
+                </tbody>
+            </table>
+        </div>
+        <!-- Order Price Detils -->
+        <div class="mb-5 px-5">
+            <div class="flex flex-row border-b border-gray-300">
+                <div class="w-6/12 px-6 py-3">
+                    <label for="">Subtotal:</label>
+                </div>
+                <div class="w-6/12 px-6 py-3">
+                    <label for="">$57.30</label>
+                </div>
+            </div>
+            <div class="flex flex-row border-b border-gray-300">
+                <div class="w-6/12 px-6 py-3">
+                    <label for="">Payment method:</label>
+                </div>
+                <div class="w-2/6/12 px-6 py-3">
+                    <label for="">Direct bank transfer</label>
+                </div>
+            </div>
+            <div class="flex flex-row ">
+                <div class="w-6/12 px-6 py-3">
+                    <label for="">Total:</label>
+                </div>
+                <div class="w-6/12 px-6 py-3">
+                    <label for="">$57.30</label>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Billing address heading -->
-    <h3 class="billing-address-heading">Billing Address</h3>
+    <div class="my-5 ">
+        <h1 class="text-5xl">
+            Billing Address
+        </h1>
+    </div>
     
     <!-- Billing address box -->
-    <div class="billing-address-box">
-        <p>Thevanankumaran Jegakumaran</p>
-        <p>fdsfsdfsf</p>
-        <p>353/8g , Mannar road , Vepankulam , Vavuniya</p>
-        <p>sfdsfdfsfsfd</p>
-        <p>Vavuniya</p>
-        <p>43000</p>
-        <p>Sri Lanka</p>
-        <p>0776999923</p>
-        <p>thevanan@yahoo.com</p>
+    <div class="text-gray-400 font-light flex flex-col border border-gray-300">
+        <div class="px-11 py-5">
+            <div>
+                <label id="txtComapnyName">Holden and Knapp Trading</label>
+            </div>
+            <div>
+                <label id="txtFirstName">Evangeline</label>
+                <label id="txtLastName">Holloway</label>
+            </div>
+            <div>
+                <label id="txtStreetAdress">94 Rocky First Street</label>
+            </div>
+            <div>
+                <label id="txt">Illo eum aut ipsam v</label>
+            </div>
+            <div>
+                <label id="txtPostCode">1343</label>
+                <label id="txtTown">Est laboriosam et</label>
+            </div>
+            <div>
+                <label id="txtCountry">Norway</label>
+            </div>
+            <div>
+                <span><i class="fa-solid fa-phone"></i></span>
+                <label id="txtPhone">2862777806</label>
+            </div>
+            <div>
+                <span><i class="fa-solid fa-envelope"></i></span>
+                <label id="txtEmail">bevemus@mailinator.com</label>
+            </div>
+        </div>
+    </div>
     </div>
 </body>
 </html>
