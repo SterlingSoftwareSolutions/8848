7854b4607f3dff774489b0a8e37b47baa6b7ac41
Mhm Nimshan <madusanka.senadhi@towcha.com.au>
2023-10-10 20:04:55 +0530

    Change Variant UI

--- resources/views/admin/product/addproduct.blade.php
+++ resources/views/admin/product/addproduct.blade.php
@@ -122,7 +122,7 @@
                 <input id="file_input" type="file" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
             </div>
         </div>
-        <h1 class=" text-[#1670B7] font-bold text-lg">Variation</h1>
+        <h1 class=" text-[#1670B7] font-bold text-lg">Add Variation</h1>
         <div class="flex flex-row py-10">
             <div class="max-w-md">
                 <div id="form-container">
