79aa04a292e9f0483d805eb192b2a28e859b98fa
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-10-11 07:07:43 +0530

    Update footer.blade.php

--- resources/views/layouts/app/footer.blade.php
+++ resources/views/layouts/app/footer.blade.php
@@ -1,28 +1,28 @@
 <footer class="bg-white shadow-inner md:mt-8" style="z-index: -999;">
-    <div class="w-full max-w-screen-xl p-4 py-8 mx-auto lg:py-16">
+    <div class="w-full p-4 py-8 mx-auto max-w-screen-3xl lg:py-16 md:ml-20">
 
         <div class="md:flex md:justify-between">
 
             <div class="mb-6 md:mb-0 md:w-1/3">
                 <a href="" class="grid items-center justify-center text-center">
                     <img class="w-24 h-24 mx-auto" src="{{ URL('images/logo.png')}}" alt="Logo Image">
-                    <p class="text-black text-center mt-4">Lorem Ipsum Dolor Sit Amet, Consectetur</p>
+                    <p class="mt-4 text-center text-black">Lorem Ipsum Dolor Sit Amet, Consectetur</p>
                 </a>
             </div>
 
-            <div class="md:w-6/12 flex flex-col md:flex-row gap-5">
-                <img class="w-1/2 h-auto md:w-1/4 md:h-[300px]" src="{{ URL('images/iphone-x-mockup.png')}}">
-                <div class="flex flex-col text-center md:text-left">
+            <div class="flex flex-col gap-5 md:w-6/12 md:flex-row">
+                <img class="w-36 h-auto md:w-1/4 md:h-[300px] md:ml-0 ml-[100px]" src="{{ URL('images/iphone-x-mockup.png')}}" style="width: 40%">
+                <div class="flex flex-col ml-8 text-center md:text-left">
                     <h1 class="text-2xl font-bold">Download</h1>
                     <h2 class="text-xl font-semibold">8848 Supplies Mobile App</h2>
-                    <p class="text-black mt-2">Lorem Ipsum Dolor Sit Amet, Consectetur<br>Adipiscing Elit. Sed Non Risus. Suspendisse<br>Lectus Tortor, Dignissim Sit Amet,</p>
-                    <img class="w-24 h-auto mx-auto mt-4" src="{{ URL('images/Download-on-the-App-Store-1.png')}}">
+                    <p class="mt-2 text-black">Lorem Ipsum Dolor Sit Amet, Consectetur<br>Adipiscing Elit. Sed Non Risus. Suspendisse<br>Lectus Tortor, Dignissim Sit Amet,</p>
+                    <img class="h-auto mx-auto mt-4 md:ml-[12px] w-28 ml-[80px]" src="{{ URL('images/Download-on-the-App-Store-1.png')}}">
                 </div>
             </div>
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:ml-20">
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:ml-20">
                 <div class="text-center sm:text-left">
-                    <h2 class="text-lg font-semibold text-blue-900 uppercase mb-2">Buyer Central</h2>
+                    <h2 class="mb-2 text-lg font-semibold text-blue-900 uppercase">Buyer Central</h2>
                     <ul class="text-gray-500">
                         <li><a href="#" class="hover:underline">Home</a></li>
                         <li><a href="#" class="hover:underline">About</a></li>
@@ -33,19 +33,19 @@
                 </div>
 
                 <div class="text-center sm:text-left">
-                    <h2 class="text-lg font-semibold text-blue-900 uppercase mb-2">Contact Info</h2>
+                    <h2 class="mb-2 text-lg font-semibold text-blue-900 uppercase">Contact Info</h2>
                     <ul class="text-gray-500">
                         <li class="flex mb-4">
                             <img class="w-5 h-5" src="{{ URL('images/location.png')}}">
-                            <a href="https://www.google.com/maps/@-26.7738869,134.7806741,12.75z?entry=ttu" class="hover:underline ml-2">Unit 3/1 Everaise Ct, Laverton North VIC 3026</a>
+                            <a href="https://www.google.com/maps/@-26.7738869,134.7806741,12.75z?entry=ttu" class="ml-2 hover:underline">Unit 3/1 Everaise Ct, Laverton North VIC 3026</a>
                         </li>
-                        <li class="flex mb-4">
+                        <li class="flex mb-4 ml-12 md:ml-0">
                             <img class="w-5 h-5" src="{{ URL('images/email.png')}}">
-                            <a href="mailto:info@carrental.com" class="hover:underline ml-2">info@8848supplies.com.au</a>
+                            <a href="mailto:info@carrental.com" class="ml-2 hover:underline">info@8848supplies.com.au</a>
                         </li>
-                        <li class="flex mb-4">
+                        <li class="flex mb-4 ml-20 md:ml-0">
                             <img class="w-5 h-5" src="{{ URL('images/telephone.png')}}">
-                            <a href="tel:8801 738 5678 64" class="hover:underline ml-2">(03) 7014 0663<br>61 1300 850 295</a>
+                            <a href="tel:8801 738 5678 64" class="ml-2 hover:underline">(03) 7014 0663<br>61 1300 850 295</a>
                         </li>
                     </ul>
                 </div>
