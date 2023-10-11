cf9ea595e7d6090a1711d13f6a024cdb155caa4a
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-10-11 15:45:33 +0530

    update index

--- resources/views/app/products/index.blade.php
+++ resources/views/app/products/index.blade.php
@@ -14,17 +14,17 @@
     <div class="container min-w-fit">
         {{-- Main Image --}}
         <div class="relative w-screen">
-            <div class="bg-cover h-56 md:h-96" style="background-image: url('{{
+            <div class="h-56 bg-cover md:h-96" style="background-image: url('{{
         asset('images/composition-cleaning-objects-with-copyspace@0.5x.jpg')
     }}'); "></div>
-            <div class="absolute inset-0 flex items-center justify-center bg-opacity-80 bg-black">
-                <h1 class="text-2xl md:text-4xl font-bold text-white underline text-center">
+            <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80">
+                <h1 class="text-2xl font-bold text-center text-white underline md:text-4xl">
                     Product
                 </h1>
             </div>
         </div>
-        <div class="flex items-center justify-center relative mx-auto ">
-            <div class="flex flex-col md:flex-row items-center justify-between w-8/12 align-middle mt-10 md:mt-4">
+        <div class="relative flex items-center justify-center mx-auto ">
+            <div class="flex flex-col items-center justify-between w-8/12 mt-10 align-middle md:flex-row md:mt-4">
                 <!-- Image Box 1 -->
                 <div class="w-full px-4 rounded-md md:w-1/3">
                     <img src="{{ asset('images/su-san-lee-g3PyXO4A0yc-unsplash.jpg') }}" alt="Image Description" class="w-full h-[150px] object-cover rounded-md">
@@ -46,11 +46,12 @@
         </div>
 
 
-        <div class="grid items-center justify-center grid-cols-1 gap-5 p-5 md:grid-cols-4 ml-0 mr-0 md:ml-[10%] md:mr-[10%] relative mx-auto">
+        <div class="relative grid items-center justify-center grid-cols-1 gap-5 p-5 mx-auto ml-0 mr-0 md:grid-cols-4 md:ml-0 md:mr-0">
             @foreach($products as $product)
             <x-product-card :product="$product" />
             @endforeach
         </div>
+        
         <!-- FOOTER -->
         @include('layouts.app.footer')
 </body>
