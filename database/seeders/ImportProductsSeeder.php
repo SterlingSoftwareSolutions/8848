b99c356a9f16d7719fa1aef9d5df1c9718431af2
Naseef <AUNaseef@users.noreply.github.com>
2023-10-12 11:28:44 +0530

    CSV IMPORT

---
+++ database/seeders/ImportProductsSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Variant;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+
+class ImportProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $file_to_read = fopen('database/csv/products.csv', 'r');
+
+        if ($file_to_read !== FALSE) {
+            while (($data = fgetcsv($file_to_read, 100, ',')) !== FALSE) {
+                if($data[0] == 'category'){
+                    continue;
+                }
+
+                $parent_cat = Category::where('name', $data[0])->first();
+
+                if(!$parent_cat){
+                    $parent_cat = Category::create([
+                        'name' => $data[0],
+                        'parent_id' => null
+                    ]);
+                }
+
+                if($data[1]){
+                    $child_cat = Category::where('name', $data[1])->first();
+                    if(!$child_cat){
+                        Category::create([
+                            'name' => $data[1],
+                            'parent_id' => $parent_cat->id
+                        ]);
+                    }
+                }
+
+                if($data[2]){
+                    $product = Product::where('title', $data[2])->first();
+
+                    if(!$product){
+                        $product = Product::create([
+                            'title' => $data[2],
+                            'category_id' => $child_cat->id ?? $parent_cat->id,
+                            'description' => '',
+                            'short_description' => '',
+                            'sku' => '',
+                        ]);
+
+                        Variant::create([
+                            'product_id' => $product->id,
+                            'name' => 'default',
+                            'price' => 0.0
+                        ]);
+                    }
+                }
+            }
+            fclose($file_to_read);
+        }
+    }
+}
