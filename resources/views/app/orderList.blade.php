d5e2b8ea4c54de161da705876b6545422e77f37e
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-10-13 12:02:26 +0530

    Update orderList.blade.php

--- resources/views/app/orderList.blade.php
+++ resources/views/app/orderList.blade.php
@@ -1,142 +1,127 @@
-@extends('layouts.app')
-@section('content')
+@extends('layouts.admin') @section('content')
+<div class="md:mb-5">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Cart</title>
-</head>
-
-<body>
-    <div class="container p-4 mx-auto mt-10 min-w-fit sm:px-6 md:px-8">
-        <!-- Navigation Bar -->
-        <div class="flex flex-col gap-1 text-sm font-bold text-blue-800 md:flex-row md:text-2xl">
-            <a href="">Home</a>
-            <span>/</span>
-            <span>Cart</span>
-        </div>
-        <!-- Alert -->
-        <div class="mt-5">
-            <!-- Error Notification -->
-
-            <div class="flex flex-col pb-5 mt-5 bg-white border-t-4 border-red-700 rounded-md shadow-lg sm:flex-row">
-                <!-- ICON -->
-                <div class="mt-5 text-red-700 md:ml-10 sm:ml-0">
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                </div>
-                <!-- Text -->
-                <div class="flex justify-start mt-5 ml-5 font-light">
-
-                </div>
-                <!-- Button -->
-                <div class="mt-3 ml-auto">
-                    <button type="button" class="text-red-700 hover:text-indigo-950 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-                        x
-                    </button>
-                </div>
-            </div>
-
-            <!-- Success Notification -->
-
-            <div class="flex flex-col my-3 bg-white border-t-4 rounded-md shadow-lg border-lime-600 sm:flex-row">
-                <!-- ICON -->
-                <div class="mt-5 md:ml-10 sm:ml-0 text-lime-600">
-                    <i class="fa-solid fa-circle-check"></i>
-                </div>
-                <!-- Text -->
-                <div class="flex justify-start mt-5 ml-5 font-light">
-
-                </div>
-                <!-- Button -->
-                <div class="mt-3 ml-auto">
-                    <button type="button" class="font-light text-red-700 hover:text-indigo-950 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-                        x
-                    </button>
-                </div>
-            </div>
+    <div class="flex flex-col items-center gap-2 mx-2 mt-5 md:flex-row md:mx-10 ">
+        <div class="flex justify-between w-full">
 
         </div>
+    </div>
+    <div class="flex flex-col items-center gap-2 mx-2 mt-5 md:flex-row md:mx-10 ">
+        <div class="flex justify-between w-full">
+            
+<form class="flex items-center gap-5">   
+    <label for="simple-search" class="sr-only">Search</label>
+    <div class="relative w-full">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
+            </svg>
+        </div>
+        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full pl-10 p-2.5  " placeholder="Search branch name..." required>
+    </div>
+    <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-900 rounded-lg border hover:bg-blue-800 focus:ring-4 focus:outline-none">
+        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+        <span class="sr-only">Search</span>
+    </button>
+    <div class="w-1/2 pr-5">
+        <select id="customer_type" class="p-2 border rounded ">
+            <option value="wholesale">Wholesale Customer</option>
+            <option value="retail">Retail Customer</option>
+        </select>
+    </div>
+    <div class="w-1/2 pr-5">
+        <select id="customer_type" class="p-2 border">
+            <option value="wholesale">High</option>
+            <option value="wholesale">Medium</option>
+            <option value="retail">Low</option>
+        </select>
+    </div>
+</form>
 
-        <form method="post" id="cart_form">
+            <a href="">
 
-            <!-- Table -->
-            <div class="relative mt-10 overflow-x-auto">
-                <table class="w-full text-xl font-light text-center text-gray-500 dark:text-gray-400">
-                    <thead class="text-lg font-light capitalize border-b text-gray-6600">
-                        <!-- Table Headings -->
-                        <tr>
-                            <th scope="col" class="w-3/12 px-2 py-3 md:w-2/12">
-                                Order ID
-                            </th>
-                            <th scope="col" class="w-1/12 px-2 py-3 md:w-2/12">
-                                Price
-                            </th>
-                            <th scope="col" class="w-2/12 px-2 py-3 md:w-2/12">
-                                Status
-                            </th>
-                            
-                            <th scope="col" class="w-2/12 px-2 py-3 md:w-2/12">
-                                Action
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="border-b">
-                            <td class="p-2">
-                                <h1 class="">#</h1>
-                            </td>
-                            <td class="p-2">
-                                <span class="">$0 .00</span>
-                            </td>
-                            <td class="p-2">
-                                <span class="p-2 text-sm font-semibold text-green-600 rounded">Approved</span>
-                            </td>
-                            
-                            <td class="flex justify-center p-2">
-                                <button class="w-1/3 p-2 text-sm text-black bg-blue-400 rounded">Edit</button>
-                                <button class="w-1/3 p-2 ml-3 text-sm text-black bg-red-600 rounded">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="p-2">
-                                <h1 class="">#</h1>
-                            </td>
-                            <td class="p-2">
-                                <span class="">$0 .00</span>
-                            </td>
-                            <td class="p-2">
-                                <span class="p-2 text-sm font-semibold text-red-600 rounded">Reject</span>
-                            </td>
-                            <td class="flex justify-center p-2">
-                                <button class="w-1/3 p-2 text-sm text-black bg-blue-400 rounded">Edit</button>
-                                <button class="w-1/3 p-2 ml-3 text-sm text-black bg-red-600 rounded">Delete</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <h1 class="px-2 py-2 text-center text-white bg-blue-900 rounded-lg ">New Order</h1>
+            </a>
+        </div>
+    </div>
+    {{-- End Dropdowns & Buttons Row --}}
+    <div class="flex flex-col mx-2 border-2 md:mt-5 md:mx-10 ">
+
+        <div class="text-blue-900">
+            <div class="flex flex-row p-5 bg-gray-200">
+                <p class="w-[15%]">Number</p>
+                <p class="w-[10%]">Date</p>
+                <p class="w-[15%]">Customer Name</p>
+                <p class="w-[10%]">Status</p>
+                <p class="w-[10%]">Items</p>
+                <p class="w-[10%]">Discount</p>
+                <p class="w-[10%]">Total</p>
+                <p class="w-[20%]">Action</p>
             </div>
+            <div class="flex flex-row items-center p-5">
+              
+                <p class="w-[10%]">#</p>
+              
+                <div class="w-[10%]">
+
+                </div>
+              
+                <div class="w-[15%]">
+
+                </div>
+              
+                <div class="w-[10%]">
+
+                    {{-- <label class="px-2 py-2 mx-auto text-center text-yellow-600 bg-yellow-200 rounded-lg"><span
+                        class="ml-2 mr-2">Pending</span>
+                    </label>
+
+
+                  <label class="px-2 py-2 mx-auto text-center text-green-600 bg-green-200 rounded-lg"><span
+                      class="ml-4 mr-4">Paid</span>
+                  </label>
+
+              
+
+                    <label class="px-2 py-2 mx-auto text-center text-red-600 bg-red-200 rounded-lg"><span
+                        class="ml-2 mr-2">unpaid</span>
+                    </label>
+
+              
+
+                    <label class="px-2 py-2 mx-auto text-center text-gray-600 bg-gray-200 rounded-lg"><span
+                        class="ml-1 mr-1"></span>
+                    </label> --}}
+
+                </div>
+              
+                <div class="w-[10%]">
+
+                </div>
+              
+                <div class="w-[10%]">
+                  0%
+                </div>
+              
+                <div class="w-[10%]">
+
+                </div>
+              
+                <div class="md:flex flex-row gap-1 w-[20%] mx-1">
+                  <div class="w-40">
+                    <h1 class="px-2 py-2 mx-auto text-center text-white bg-green-600 rounded-lg">Approve </h1>
+                  </div>
+                  <div class="w-40">
+                    <h1 class="px-2 py-2 mx-auto text-center text-white bg-red-600 rounded-lg">Cancel </h1>
+                  </div>
+                  <div class="w-40">
+                    <a href="/admin/orders//edit"><h1 class="px-2 py-2 mx-auto text-center text-white bg-black rounded-lg">Edit </h1></a>
+                  </div>
+                </div>
+              </div>
+        </div>
     </div>
-    </form>
-    </div>
-
-</body>
-<script>
-    // JavaScript to handle increment and decrement actions
-    const inputElement = document.querySelector('input[name="custom-input-number"]');
-    const incrementButton = document.querySelector('[data-action="increment"]');
-    const decrementButton = document.querySelector('[data-action="decrement"]');
-
-    incrementButton.addEventListener('click', () => {
-        inputElement.value = parseInt(inputElement.value) + 1;
-    });
-
-    decrementButton.addEventListener('click', () => {
-        const value = parseInt(inputElement.value);
-        if (value > 0) {
-            inputElement.value = value - 1;
-        }
-    });
-</script>
+</div>
 @endsection
