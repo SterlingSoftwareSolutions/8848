ccb6029089c18acc5df9c9ec32cc63d6ce99817f
Mhm Nimshan <madusanka.senadhi@towcha.com.au>
2023-10-13 14:04:09 +0530

    Change Category add page

--- resources/views/admin/category/addCategory.blade.php
+++ resources/views/admin/category/addCategory.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.admin') @section('content')
 
 <div class="p-8">
-    <h1 class=" text-[#1670B7] font-bold text-lg">Add Category</h1>
+    <h1 class=" text-[#1670B7] font-bold text-lg pb-10">Add Category</h1>
     <form>
-        <section class="w-full py-10">
+        <div class="flex flex-col gap-6 mb-6 md:grid-cols-2 w-full">
+            <div class="flex flex-row gap-5">
+                <div class="flex flex-col w-full">
+                    <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parent Category * :</label>
+                    <select type="text" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option>Sugarcane</option>
+                        <option>Sugarcane1</option>
+                        <option>Sugarcane2</option>
+                        <option>Sugarcane4</option>
+                        <option>Sugarcane3</option>
+                    </select>
+                </div>
+                <div class="flex flex-col w-full"> <!-- Add w-full class here to make it full width -->
+                    <label for="category_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Name * :</label>
+                    <input type="text" id="category_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
+                </div>
+            </div>
+            <div class="flex flex-row gap-5">
+                <div class="w-full">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Category Icon</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="file_input" type="file">
+                </div>
+                <div class="w-full">
+                    <label for="category_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Description * :</label>
+                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Your message..."></textarea>
+                </div>
+            </div>
+        </div>
+        <section class="w-full pb-5 -mt-10">
             <div class="max-w-sm bg-white rounded-lg shadow-md overflow-hidden">
                 <div class="px-4 py-6">
                     <div id="image-preview" class="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg mx-auto text-center cursor-pointer">
@@ -21,31 +49,6 @@
                 </div>
             </div>
         </section>
-        <div class="flex flex-col gap-6 mb-6 md:grid-cols-2">
-            <div>
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Category Icon</label>
-                <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="file_input" type="file">
-            </div>
-            <div>
-                <label for="category_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Name * :</label>
-                <input type="text" id="category_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
-            </div>
-            <div>
-                <label for="category_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Description * :</label>
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Your message..."></textarea>
-            </div>
-            <div>
-                <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parent Category * :</label>
-                <select type="text" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option>Sugarcane</option>
-                    <option>Sugarcane1</option>
-                    <option>Sugarcane2</option>
-                    <option>Sugarcane4</option>
-                    <option>Sugarcane3</option>
-                </select>
-            </div>
-        </div>
-
         <button type="submit" class="text-white bg-[#2BB673] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal text-base w-full sm:w-auto px-12 py-2.5 text-center">Save</button>
         <button type="submit" class="text-white bg-[#D2042D] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal text-base w-full sm:w-auto px-6 py-2.5 text-center ">Cancel</button>
     </form>
