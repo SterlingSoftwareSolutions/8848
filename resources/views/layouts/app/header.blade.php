cd1d4c8db8463ae3facf706f28cf54570995187a
Naseef <AUNaseef@users.noreply.github.com>
2023-10-31 18:00:14 +0530

    Shop submenu dropdown

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -52,7 +52,16 @@
               <div class="flex space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="/" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500">Home</a>
-                <a href="/categories" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500">Shop</a>
+                <div class="my-auto relative z-10">                  
+                  <a href="/categories" class="peer px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500">Shop</a>
+                  <div class="absolute hidden peer-hover:block right-0 hover:block">                    
+                    <div class="w-64 py-1 mt-3 bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      @foreach(App\Models\Category::with('children')->where('parent_id', null)->get(); as $category)
+                      <a href="/categories/{{$category->id}}" class="block px-4 py-2 text-sm text-gray-700 hover:text-blue-500">{{$category->name}}</a>
+                      @endforeach
+                    </div>
+                  </div>
+                </div>
                 <a href="/my-list" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500" role="menuitem" tabindex="-1" id="user-menu-item-1">My List</a>
                 <a href="/contact" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500">Contact</a>
                 @if(Auth::check() && (Auth::user()->role == 'admin' || Auth::user()->role == 'superadmin'))
