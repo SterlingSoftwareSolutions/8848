cbe8034015bb5bafa7a162c1cbe15daf0eb5d7a1
Naseef <AUNaseef@users.noreply.github.com>
2023-12-14 11:58:03 +0530

    Remove Category Icons

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -3,9 +3,8 @@
 ])
 <a href="/categories/{{$category->id}}">
 <div class="w-full aspect-square bg-gray-200 -skew-x-6 rounded-md bg-cover" style="background-image: url({{$category->background}});">
-    <div class="bg-gray-200 w-full h-full bg-opacity-100 text-blue-800 hover:bg-black [&>.image]:hover:invert hover:bg-opacity-50 hover:text-white flex flex-col items-center justify-center">
-        <img src="{{$category->icon}}" class="w-1/3 aspect-square object-contain" alt="">
-        <h1 class="text-center text-lg p-2">
+    <div class="rounded-md bg-blue-900 w-full h-full bg-opacity-5 text-white hover:text-opacity-100 [&:not(:hover)>h1]:truncate [&:not(:hover)>h1]:backdrop-blur-sm [&>h1]:hover:bg-opacity-0 hover:bg-opacity-30 hover:backdrop-blur-sm flex flex-col items-center justify-end hover:justify-center overflow-hidden">
+        <h1 class="text-center  bg-blue-900 bg-opacity-40 w-full text-base p-2">
             {{$category->name}}
         </h1>
     </div>
