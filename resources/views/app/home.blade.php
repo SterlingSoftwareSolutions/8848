e1084379d82415803a5cabcb4cf6be11f1a1e6b1
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-12-20 08:21:30 +0530

    fixes the issues UI

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -65,7 +65,7 @@
     <div class="flex flex-col py-5 md:flex-row">
         <!-- Mug Image -->
         <div class="relative md:w-2/12">
-            <div class="absolute inset-0 bg-opacity-50 bg-no-repeat -mt-24 md:-ml-36" style="background-image: url('{{ asset('images/jug-image.png') }}'); opacity: 0.5;"></div>
+            <div class="absolute inset-0 -mt-24 bg-opacity-50 bg-no-repeat md:-ml-36" style="background-image: url('{{ asset('images/jug-image.png') }}'); opacity: 0.5;"></div>
         </div>
 
         <!-- Text -->
@@ -73,9 +73,9 @@
             <div class="ml-5">
                 <h1 class="mb-5 text-4xl text-white">We Are 8848 Suppliers</h1>
                 <p class="mb-5 text-white">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sed Et Leo Tincidunt, Placerat Ex In, Feugiat Urna. Integer Dictum Tellus Vitae Turpis Consequa</p>
-                <div class="flex items-center justify-center md:items-start md:justify-start">
+                {{-- <div class="flex items-center justify-center md:items-start md:justify-start">
                     <a href="/categories" class="text-blue-600 bg-white font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 uppercase flex md:justify-center md:items-center hover:bg-blue-300">shop now</a>
-                </div>
+                </div> --}}
             </div>
         </div>
 
@@ -94,7 +94,7 @@
                                 <!-- Original image -->
                                 <div class="relative">
                                     <!-- Original Image -->
-                                    <img src="{{ asset('images/Path.png') }}" alt="Original Image" class="w-full h-[280px] object-cover transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+                                    <img src="{{ $category['Background'] }}" alt="Original Image" class="w-full h-[280px] object-cover transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
 
                                     <!-- Overlay Image and Text -->
                                     <div class="absolute inset-0 flex flex-col items-center justify-center group-hover:opacity-100">
@@ -109,7 +109,7 @@
                                 </div>
                                 <!-- Hover image (Initial) -->
                                 <div class="hover-image">
-                                    <img src="{{ asset('images/product-dummy.jpeg') }}" alt="Hover Image" class="hover_img w-full h-[280px] object-cover absolute top-0 left-0 transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-50">
+                                    <img src="{{ $category['Background'] }}" alt="Hover Image" class="hover_img w-full h-[280px] object-cover absolute top-0 left-0 transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-50">
 
                                     <!-- Icon and text -->
                                     <div class="absolute inset-0 flex flex-col items-center justify-center group-hover:opacity-100">
@@ -138,34 +138,33 @@
     <!-- Text -->
     <div class="text-center">
         <h1 class="my-5 text-2xl tracking-wide text-blue-500">Secure And Organize</h1>
-        <h1 class="my-5 text-3xl font-bold tracking-wide text-blue-500">Explore Our Range Of Reliable And Innovative Solutions</h1>
-        <p class="my-5 text-lg font-light tracking-wider text-gray-600">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sed Et Leo Tincidunt, Placerat Ex In, Feugiat Urna. Integer Dictum Tellus Vitae Turpis Consequat,</p>
+        <h1 class="my-5 text-3xl font-bold tracking-wide text-blue-500">We are 8848 Supplies</h1>
+        <p class="my-5 text-lg font-light tracking-wider text-gray-600 md:mx-12">With five years of excellence, 8848 Supplies is a leading provider in cleaning supplies and food packaging. Serving top names in hospitality, our commitment to quality and innovation ensures pristine environments and flawlessly packaged culinary experiences. Experience the pinnacle of excellence with 8848 Supplies</p>
     </div>
 
     <!-- Icons -->
-    <div class="py-4  w-6/12  mx-auto bg-[#166EB6] flex flex-col md:flex-row  justify-around md:py-10 md:px-10">
-        <div class="flex flex-col items-center justify-center md:flex-row md:border-r-2 ">
+    <div class="py-4  w-9/12  mx-auto bg-[#4b98d82f] flex flex-col md:flex-row  justify-around md:py-10 md:px-10">
+        <div class="flex flex-col items-center justify-center md:flex-row">
             <div class="flex flex-col items-center w-1/3 ">
                 <div class="">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="blue" class="w-20 h-20">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                     </svg>
-                </div>
+                </div>                
                 <div class="text-center">
-                    <h1 class=" text-[20px] md:text-[30px] text-white">Quick Delivery</h1>
+                    <h1 class=" text-[20px] md:text-[30px] text-blue-700 md:w-full">Quick Delivery</h1>
                 </div>
             </div>
-
         </div>
         <div class="flex flex-col items-center justify-center md:flex-row ">
             <div class="flex flex-col items-center w-1/3">
                 <div class="">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="blue" class="w-20 h-20">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
-                    </svg>
+                    </svg>                    
                 </div>
                 <div class="text-center">
-                    <h1 class=" text-[20px] md:text-[30px] text-white">Secure Payment</h1>
+                    <h1 class=" text-[20px] md:text-[30px]  text-blue-700 md:w-full">Secure Payment</h1>
                 </div>
 
             </div>
@@ -174,12 +173,12 @@
         <div class="flex flex-col items-center justify-center md:flex-row md:border-l-2">
             <div class="flex flex-col items-center w-1/3">
                 <div class="">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="blue" class="w-20 h-20">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.746 3.746 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
-                    </svg>
+                    </svg>                    
                 </div>
                 <div class="text-center">
-                    <h1 class=" text-[20px] md:text-[30px] text-white">Best Quality</h1>
+                    <h1 class=" text-[20px] md:text-[30px] text-blue-700 md:w-full">Best Quality</h1>
                 </div>
             </div>
 
