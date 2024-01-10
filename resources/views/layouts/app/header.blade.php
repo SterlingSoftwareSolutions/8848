0b21364906d13c418cbb1f8ea0b293bbeb3294da
Naseef <AUNaseef@users.noreply.github.com>
2024-01-10 12:53:19 +0530

    Show logged in user's name

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -93,8 +93,8 @@
           </div>
 
           @if(Auth::check())
-          <button id="user-icon-button" type="button" class="relative h-10 ml-4 px-3 py-1.5 text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500">
-            <span class="absolute -inset-1.5"></span>
+          <button id="user-icon-button" type="button" class="flex justify-center items-center gap-2 relative h-10 ml-4 px-3 py-1.5 text-gray-300 rounded-md hover:bg-blue-200 hover:text-blue-500">
+            <p class="text-sm font-medium">{{ auth()->user()->first_name }}</p>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
@@ -112,9 +112,6 @@
               <button href="/orders" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:text-blue-500" role="menuitem" tabindex="-1" id="user-menu-item-1">Logout</button>
             </form>
           </div>
-          @auth
-            <p class="text-white">Hi, {{ auth()->user()->first_name }}</p>
-          @endauth
         </div>
       </div>
     </div>
