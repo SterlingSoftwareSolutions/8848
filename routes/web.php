1158867b364393876647087ff121b196e3fc33b8
Naseef <AUNaseef@users.noreply.github.com>
2024-02-27 18:03:02 +0530

    Password Reset

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,10 @@ Route::get('/login', [AuthController::class, 'login_form'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'register_form'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/forgot-password', [AuthController::class, 'reset_passsword_form'])->name('forgot-password');
-Route::post('/forgot-password', [AuthController::class, 'reset_passsword'])->name('password.reset');
+Route::get('/forgot-password', [AuthController::class, 'forgot_passsword_form'])->name('password.forgot');
+Route::post('/forgot-password', [AuthController::class, 'forgot_passsword'])->name('password.forgot');
+Route::get('/reset-password', [AuthController::class, 'reset_passsword_form'])->name('password.reset');
+Route::post('/reset-password', [AuthController::class, 'reset_passsword'])->name('password.reset');
 
 // Public Routes
 Route::get('/', [HomeController::class, 'index']);
